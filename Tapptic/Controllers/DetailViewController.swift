5bdfd441e73a0f0ab09e0e4647e79f0a71134f82
Krzysztof Lech <krzysztof.lech@24abc.pl>
2018-10-22 08:12:25 +0200

    Split View Controller added

---
+++ Tapptic/Controllers/DetailViewController.swift
@@ -0,0 +1,16 @@
+//
+//  DetailViewController.swift
+//  Tapptic
+//
+//  Created by Krzysztof Lech on 22/10/2018.
+//  Copyright © 2018 Krzysztof Lech. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ Tapptic/Controllers/MasterViewController.swift
@@ -0,0 +1,35 @@
+//
+//  MasterViewController.swift
+//  Tapptic
+//
+//  Created by Krzysztof Lech on 22/10/2018.
+//  Copyright © 2018 Krzysztof Lech. All rights reserved.
+//
+
+import UIKit
+
+class MasterViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
+// MARK: - Table View Data Souce Methods
+
+extension MasterViewController {
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 0
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MasterVCcell", for: indexPath)
+        
+        return cell
+    }
+}
