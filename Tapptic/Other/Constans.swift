4cdb97464ab6b614879fa0f5182c71d10b09678f
Krzysztof Lech <krzysztof.lech@24abc.pl>
2018-10-22 08:28:20 +0200

    Constants struct added

---
+++ Tapptic/Other/Constans.swift
@@ -0,0 +1,15 @@
+//
+//  Constans.swift
+//  Tapptic
+//
+//  Created by Krzysztof Lech on 22/10/2018.
+//  Copyright © 2018 Krzysztof Lech. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    
+    static let itemsUrl = "http://dev.tapptic.com/test/json.php"
+    static let itemDetailslUrl = "http://dev.tapptic.com/test/json.php?name="
+}
