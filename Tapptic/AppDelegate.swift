18c5ac00bad5c6af30ac613e097bfa24529f57dc
Krzysztof Lech <krzysztof.lech@24abc.pl>
2018-10-24 07:03:43 +0200

    Small refactor

--- Tapptic/AppDelegate.swift
+++ Tapptic/AppDelegate.swift
@@ -13,20 +13,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
         guard
             let splitViewController = window?.rootViewController as? UISplitViewController,
-            let masterNavigationController = splitViewController.viewControllers.first as? UINavigationController,
-            let masterViewController = masterNavigationController.topViewController as? MasterViewController,
             let detailNavigationController = splitViewController.viewControllers.last as? UINavigationController,
             let detailViewController = detailNavigationController.topViewController as? DetailViewController
             else { fatalError() }
-        
-        /// przekazać początkowe dane DetailVC
-        
-        /// dodać delegata do MasterVC
         
         // iPad settings
         detailViewController.navigationItem.leftItemsSupplementBackButton = true
